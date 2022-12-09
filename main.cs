aab5f6772c52f8efd2c847ea3a506a7ca0f5f9dc
Luiz Fernando Fernando <119373008+lodsilva1@users.noreply.github.com>
2022-12-09 00:42:55 +0000

    alterado o codigo

--- main.cs
+++ main.cs
@@ -4,23 +4,23 @@ class Program {
    public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
 string entrada;
-    Double m1, valor ;
+    Double qtd, valor ;
     
     Console.WriteLine("Digite quantidade de maçãs");
   
     entrada = Console.ReadLine();
 
 
-   m1 =  Double.Parse(entrada);
+   qtd =  Double.Parse(entrada);
     
-if(m1>=12){
-    valor = (m1*1.00);
+if(qtd>=12){
+    valor = (qtd*1.00);
     
     Console.WriteLine ("As maçãs custam  " + valor );
     }
       
 else{
-   valor = (m1*1.30);
+   valor = (qtd*1.30);
    Console.WriteLine ("As maçãs custam  " + valor );
 }
     
